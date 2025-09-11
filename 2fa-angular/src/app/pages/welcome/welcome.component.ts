43a6946093d51b64de39c7c7a3c1b416f260fa19
Madalina294 <puicanmadalinamaria@gmail.com>
2025-09-11 21:29:12 +0300

    Improved design

--- 2fa-angular/src/app/pages/welcome/welcome.component.ts
+++ 2fa-angular/src/app/pages/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {StorageService} from '../../services/storage/storage.service';
 import {NgIf} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -12,7 +13,7 @@ export class WelcomeComponent implements OnInit {
   user: any = null;
   userImage: string | null = null;
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
@@ -22,6 +23,7 @@ export class WelcomeComponent implements OnInit {
 
   logout(){
     StorageService.signout();
+    this.router.navigateByUrl("/login")
   }
 
 }
